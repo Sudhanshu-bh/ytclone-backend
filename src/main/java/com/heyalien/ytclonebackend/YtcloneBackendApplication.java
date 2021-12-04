8cf78e3e0cfe972339232275c86ea0443f90c21b
SUDHANSHU <bsudhanshu3@gmail.com>
2021-12-04 14:46:13 +0530

    initial commit

---
+++ src/main/java/com/heyalien/ytclonebackend/YtcloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.heyalien.ytclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtcloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtcloneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heyalien/ytclonebackend/YtcloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyalien.ytclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtcloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
